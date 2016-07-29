f135a132f4c47b83626c4468a79f08aa5c3efc44
Dan Pitic <dpitic@gmail.com>
2016-07-29 18:11:14 +0800

    Completed Point class.

--- ch02/point.py
+++ ch02/point.py
@@ -1,7 +1,14 @@
 import math
 
 class Point:
-    '''Point class represents a point in 2D.'''
+    '''Point class represents a point in 2D geometric coordinates.'''
+
+    def __init__(self, x=0, y=0):
+    	'''
+    	Initialise the Point object to the specified coordinates. Default
+    	position is the origin.
+    	'''
+    	self.move(x, y)
     
     def move(self, x, y):
         '''Move the point to the given coordinates.'''
@@ -15,7 +22,7 @@ class Point:
     def calculate_distance(self, other_point):
         '''
         Calculate the distance to the specified point using the Pythagoras
-        Theorem.
+        Theorem. The distance is returned as a float.
         '''
         return math.sqrt(
             (self.x - other_point.x)**2 +
