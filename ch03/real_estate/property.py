296685f8075d2d8b412f70c3c57d27b158105dcc
Dan Pitic <dpitic@gmail.com>
2016-08-06 10:40:41 +0800

    Completed real estate program. Next: chapter 4.

--- ch03/real_estate/property.py
+++ ch03/real_estate/property.py
@@ -1,3 +1,7 @@
+# Constant declarations
+COLS = 80       # Maximum number of text display columns
+
+
 class Property:
     """
     Superclass representing the entities managed by the real estate program.
@@ -12,7 +16,7 @@ class Property:
 
     def display(self):
         print("PROPERTY DETAILS")
-        print("==================")
+        print("=" * COLS)
         print("square footage: {}".format(self.square_feet))
         print("bedrooms:       {}".format(self.num_bedrooms))
         print("bathrooms:      {}".format(self.num_baths))
@@ -33,7 +37,7 @@ def get_valid_input(input_string, valid_options):
     """
     Obtain and validate input from the user.
     :param input_string: Prompt string.
-    :param valid_options: Set of valid input option strings.
+    :param valid_options: Tuple of valid input option strings.
     :return: A valid input option string.
     """
     input_string += " ({}) ".format(", ".join(valid_options))
@@ -44,9 +48,7 @@ def get_valid_input(input_string, valid_options):
 
 
 class Apartment(Property):
-    """
-    Property subclass representing an apartment.
-    """
+    """Property subclass representing an apartment."""
     valid_laundries = ("coin", "ensuite", "none")
     valid_balconies = ("yes", "no", "solarium")
 
@@ -58,7 +60,7 @@ class Apartment(Property):
     def display(self):
         super(Apartment, self).display()
         print("APARTMENT DETAILS")
-        print("=================")
+        print("=" * COLS)
         print("laundry:     %s" % self.laundry)
         print("has balcony: %s" % self.balcony)
         print()
@@ -81,9 +83,7 @@ class Apartment(Property):
 
 
 class House(Property):
-    """
-    Property subclass representing a house.
-    """
+    """Property subclass representing a house."""
     valid_garage = ("attached", "detached", "none")
     valid_fenced = ("yes", "no")
 
@@ -96,7 +96,7 @@ class House(Property):
     def display(self):
         super(House, self).display()
         print("HOUSE DETAILS")
-        print("================")
+        print("=" * COLS)
         print("# of stories: {}".format(self.num_stories))
         print("garage:       {}".format(self.garage))
         print("fenced yard:  {}".format(self.fenced))
@@ -118,9 +118,7 @@ class House(Property):
 
 
 class Purchase:
-    """
-    Class used to mark a property for purchase.
-    """
+    """Class used to mark a property for purchase."""
 
     def __init__(self, price='', taxes='', **kwargs):
         super(Purchase, self).__init__(**kwargs)
@@ -130,7 +128,7 @@ class Purchase:
     def display(self):
         super(Purchase, self).display()
         print("PURCHASE DETAILS")
-        print("===================")
+        print("=" * COLS)
         print("selling price:   {}".format(self.price))
         print("estimated taxes: {}".format(self.taxes))
         print()
@@ -144,9 +142,7 @@ class Purchase:
 
 
 class Rental:
-    """
-    Class used to mark a property for rental.
-    """
+    """Class used to mark a property for rental."""
 
     def __init__(self, furnished='', utilities='', rent='', **kwargs):
         super(Rental, self).__init__(**kwargs)
@@ -157,7 +153,7 @@ class Rental:
     def display(self):
         super(Rental, self).display()
         print("RENTAL DETAILS")
-        print("=======================")
+        print("=" * COLS)
         print("rent:                {}".format(self.rent))
         print("estimated utilities: {}".format(self.utilities))
         print("furnished:           {}".format(self.furnished))
@@ -173,10 +169,23 @@ class Rental:
     prompt_init = staticmethod(prompt_init)
 
 
+# Implementation note: The order of the inherited classes is important! If the
+# class was defined as class HouseRental(House, Rental), then display() would
+# not have called Rental.display().
+# When display() is called on the version below, it refers to Rental.display()
+# which calls super.display() to get the House.display() which again calls
+# super.display() to get the Property.display(), and everything works as it
+# should.
+# If the subclass arguments were reversed, display() would refer to
+# House.display(). When super() is called, it calls the method on the Property
+# parent class, but Property.display() does not have a call to super(). This
+# means Rental.display() would not be called.
+# By placing the inheritance list in the order below, it ensures that Rental
+# calls super() which initialises the House side of the hierarchy.
+# Another way to fix this would be to make Rental and Purchase subclasses of
+# Property (or modify the method resolution order dynamically).
 class HouseRental(Rental, House):
-    """
-    Class used to represent a rental hosue.
-    """
+    """Class used to represent a rental house."""
 
     def prompt_init():
         init = House.prompt_init()
@@ -186,4 +195,72 @@ class HouseRental(Rental, House):
     prompt_init = staticmethod(prompt_init)
 
 
-# Implement class ApartmentRental
+class ApartmentRental(Rental, Apartment):
+    """Class used to represent a rental apartment."""
+
+    def prompt_init():
+        init = Apartment.prompt_init()
+        init.update(Rental.prompt_init())
+        return init
+
+    prompt_init = staticmethod(prompt_init)
+
+
+class ApartmentPurchase(Purchase, Apartment):
+    """Class used to represent an apartment for sale."""
+
+    def prompt_init():
+        init = Apartment.prompt_init()
+        init.update(Purchase.prompt_init())
+        return init
+
+    prompt_init = staticmethod(prompt_init)
+
+
+class HousePurchase(Purchase, House):
+    """Class used to represent a house for sale."""
+
+    def prompt_init():
+        init = House.prompt_init()
+        init.update(Purchase.prompt_init())
+        return init
+
+    prompt_init = staticmethod(prompt_init)
+
+
+class Agent:
+    """
+    Agent class is responsible for creating new listings and displaying
+    existing ones.
+    """
+    # Dictionary class variable to map the property types to the subclasses. The
+    # keys are tuples of 2 distinct strings representing the subclasses as the
+    # values in the dictionary.
+    type_map = {
+        ("house", "rental"): HouseRental,
+        ("house", "purchase"): HousePurchase,
+        ("apartment", "rental"): ApartmentRental,
+        ("apartment", "purchase"): ApartmentPurchase
+    }
+
+    def __init__(self):
+        self.property_list = []
+
+    def display_properties(self):
+        for property in self.property_list:
+            property.display()
+
+    def add_property(self):
+        property_type = get_valid_input(
+            "What type of property? ",
+            ("house", "apartment")).lower()
+        payment_type = get_valid_input(
+            "What payment type? ",
+            ("purchase", "rental")).lower()
+
+        # Look up the class in the dictionary.
+        property_class = self.type_map[(property_type, payment_type)]
+        # Get the required input from the user (to create the property subclass)
+        init_args = property_class.prompt_init()
+        # Add the appropriate property object to the property list.
+        self.property_list.append(property_class(**init_args))
