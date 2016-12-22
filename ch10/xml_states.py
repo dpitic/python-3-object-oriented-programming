bc60d4694fd90f29c6f6ab52473595547cc547be
Dan Pitic <dpitic@gmail.com>
2016-12-22 20:48:17 +0800

    Completed state pattern XML parser. Up Next: the singleton pattern.

--- ch10/xml_states.py
+++ ch10/xml_states.py
@@ -35,16 +35,90 @@ class Node:
 
 class FirstTag:
     """
-    Initial state of the Parser.
+    Initial state of the Parser. It finds the first tag.
+    """
+    def process(self, remaining_string, parser):
+        """
+        Process the remaining XML string using the parser and return what's
+        left. This method sets the parser state to determine the next state.
+        :param remaining_string: remaining XML string.
+        :param parser: XML parser
+        :return: the remaining XML string to parse.
+        """
+        # Find the index of the opening and closing angle brackets
+        i_start_tag = remaining_string.find('<')
+        i_end_tag = remaining_string.find('>')
+        # Get the tag name between opening and closing angle brackets
+        tag_name = remaining_string[i_start_tag + 1:i_end_tag]
+        # Create the root node based on the first tag name
+        root = Node(tag_name)
+        # The root node is also the current node
+        parser.root = parser.current_node = root
+        parser.state = ChildNode()  # Determine next transition state
+        return remaining_string[i_end_tag + 1:]
+
+
+class ChildNode:
+    """
+    Determine and set the parser the next state.
+    """
+    def process(self, remaining_string, parser):
+        stripped = remaining_string.strip()
+        if stripped.startswith('</'):
+            parser.state = CloseTag()
+        elif stripped.startswith('<'):
+            parser.state = OpenTag()
+        else:
+            parser.state = TextNode()
+        return stripped     # string of text
+
+
+class OpenTag:
+    """
+    This state adds the newly created node to the previous current node object's
+    children and sets it as the new current node. The parser state is set back
+    to the ChildNode state before continuing.
     """
     def process(self, remaining_string, parser):
         i_start_tag = remaining_string.find('<')
         i_end_tag = remaining_string.find('>')
-        tag_name = remaining_string[i_start_tag + 1:i_end_tag]
-        root = Node(tag_name)
-        parser.root = parser.current_node = root
-        parser.state = ChildNode()  # Determine next transition state
-        return remaining_string[i_end_tag + 1:]
+        tag_name = remaining_string[i_start_tag+1:i_end_tag]
+        node = Node(tag_name, parser.current_node)
+        parser.current_node.children.append(node)
+        parser.current_node = node
+        parser.state = ChildNode()      # Determine next transition state
+        return remaining_string[i_end_tag+1:]
+
+
+class TextNode:
+    """
+    This state extracts the text before the next closing tag and sets it as a
+    value on the current node.
+    """
+    def process(self, remaining_string, parser):
+        # Find the end index of the closing tag
+        i_start_tag = remaining_string.find('<')
+        # Extract the text in this tag
+        text = remaining_string[:i_start_tag]
+        parser.current_node.text = text
+        parser.state = ChildNode()      # Determine next transition state
+        return remaining_string[i_start_tag:]
+
+
+class CloseTag:
+    """
+    This state sets the parser's current node back to the parent node so any
+    further children in the outside tag can be added to it.
+    """
+    def process(self, remaining_string, parser):
+        i_start_tag = remaining_string.find('<')
+        i_end_tag = remaining_string.find('>')
+        assert remaining_string[i_start_tag+1] == '/'
+        tag_name = remaining_string[i_start_tag+2:i_end_tag]
+        assert tag_name == parser.current_node.tag_name
+        parser.current_node = parser.current_node.parent
+        parser.state = ChildNode()      # Determine next transition state
+        return remaining_string[i_end_tag+1:].strip()
 
 
 class Parser:
@@ -86,5 +160,20 @@ class Parser:
         if remaining:
             self.process(remaining)
 
-    def start(self):
-        self.process(self.parse_string)
+    if __name__ == '__main__':
+        def start(self):
+            self.process(self.parse_string)
+
+# Test the API. Open a file from the command line, parse it and print the nodes
+if __name__ == '__main__':
+    import sys
+    with open(sys.argv[1]) as file:
+        contents = file.read()
+        p = Parser(contents)
+        p.start()
+
+        nodes = [p.root]
+        while nodes:
+            node = nodes.pop(0)
+            print(node)
+            nodes = node.children + nodes
